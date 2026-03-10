e0ec9d6832a874dbe3847cf20e028df098fa988f
paxpor <pporter@byupathway.edu>
2026-03-10 13:56:45 -0600

    Committed changes to week01/Exercise1/Program.cs

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your firstname? ");
+        string firstName = Console.ReadLine();
+
+        Console.WriteLine("What is your lastname? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Hello {firstName} {lastName}!");
     }
 }
